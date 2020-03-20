fbed1b6c4b45e2f686bb11fe4cca63b802963ba3
raosan <raosan@hyperjump.tech>
2020-03-20 14:14:32 +0700

    add LocationListener functions

--- app/src/main/java/tech/hyperjump/runningapp/MapsActivity.kt
+++ app/src/main/java/tech/hyperjump/runningapp/MapsActivity.kt
@@ -55,4 +55,20 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener {
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
+
+    override fun onLocationChanged(location: Location?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onProviderEnabled(provider: String?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onProviderDisabled(provider: String?) {
+        TODO("Not yet implemented")
+    }
 }
