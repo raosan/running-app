cdfd0f2971c11081516871ade1ae4c9e17e2af28
raosan <raosan@hyperjump.tech>
2020-03-22 15:46:06 +0700

    setup onCreate

--- app/src/main/java/tech/hyperjump/runningapp/InfiniteService.kt
+++ app/src/main/java/tech/hyperjump/runningapp/InfiniteService.kt
@@ -1,5 +1,7 @@
 package tech.hyperjump.runningapp
 
+import android.app.Notification
+import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
@@ -9,6 +11,7 @@ import java.lang.IllegalStateException
 const val CHANNEL_ID = "NotificationChannel"
 const val CHANNEL_NAME = "BestChannelEver"
 const val FOREGROUND_ID= 1
+const val REQUEST_CODE_NOTIFICATION = 0
 
 const val FETCH_DATA_INTERVAL = 0
 
@@ -19,13 +22,32 @@ class InfiniteService: Service() {
     var isServiceRunning = false
     lateinit var  wakeLock: PowerManager.WakeLock
 
+    override fun onCreate() {
+        super.onCreate()
+        createNoticationChannel()
+
+        val mapsActivityIntent = Intent(this, MapsActivity::class.java).let {
+            PendingIntent.getActivity(this, REQUEST_CODE_NOTIFICATION, it, 0)
+        }
+
+        startForeground(FOREGROUND_ID, getNotification(mapsActivityIntent))
+    }
+
+    private fun getNotification(mapsActivityIntent: PendingIntent?): Notification? {
+        TODO("Not yet implemented")
+    }
+
+    private fun createNoticationChannel() {
+        TODO("Not yet implemented")
+    }
+
     override fun onBind(intent: Intent?) = null
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         when (intent?.action) {
             ACTION_START -> start()
             ACTION_STOP -> stop()
-            else -> throw IllegalStateException("Action can onle be $ACTION_START or $ACTION_STOP")
+            else -> throw IllegalStateException("Action can only be $ACTION_START or $ACTION_STOP")
         }
         return START_STICKY
     }
